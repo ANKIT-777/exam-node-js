5ab5a0be78161bf0b574962345e15776b51263a7
Ankit Sharma <an2ki4t5s@gmail.com>
2023-06-13 10:36:17 +0530

    using two function made multiplication function

---
+++ question16.js
@@ -0,0 +1,15 @@
+// Question: Write a JavaScript function multiplyByTwo that takes a number as an argument and multiplies it by two. Implement a callback function callback that receives the result of the multiplication and displays it on the console.
+// Example Input: multiplyByTwo(5, callback);
+
+function multiplyByTwo(number, callback) {
+    var result = number * 2;
+    callback(result);
+  }
+  
+  function callback(result) {
+    console.log("Result: " + result);
+  }
+  
+  
+  multiplyByTwo(5, callback);
+  
