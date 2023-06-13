a8914e3f9da36efea84dc55414a070a7aea9eb13
Ankit Sharma <an2ki4t5s@gmail.com>
2023-06-13 08:41:38 +0530

    solved quesiton no. 2

---
+++ question2.js
@@ -0,0 +1,12 @@
+// Question: Write a JavaScript program that uses a while loop to calculate the sum of numbers from 1 to 10.
+// Example:
+// Input: None
+// Expected Output: 55
+var i = 1;
+var sum = 0;
+while (i < 11) {
+    sum = sum + i;
+    i++;
+}
+
+console.log(sum);
