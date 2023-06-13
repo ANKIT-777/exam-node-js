03cb1805985e79313f268a7975736bbb813402be
Ankit Sharma <an2ki4t5s@gmail.com>
2023-06-13 08:33:44 +0530

    1 question done

---
+++ question1.js
@@ -0,0 +1,8 @@
+// Question: Write a JavaScript program that uses a for loop to print numbers from 1 to 10.
+// Example:
+// Input: None
+// Expected Output: 1 2 3 4 5 6 7 8 9 10
+
+for (var i = 1; i <11; i++) {
+    console.log(i);
+}
